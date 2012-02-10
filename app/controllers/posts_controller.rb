61749e109b081c512d769d0423ad42d0fc1a33d4
Christopher Nguyen <christoph.nguyen@gmail.com>
2012-02-10 09:24:04 -0800

    Posts controller now has a barebones layout for CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  # before_filter :get_userId, :only => [:new]
+#   
+  # def get_userId
+    # @userId = User.find(params[:username]).id
+  # end
+  
+  def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html #show.html.erb
+      format.json { render :json => @post }
+    end
+  end
+  
+  def index
+    @posts = Post.all
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render :json => @post }
+    end 
+  end
+  
+  def create
+    user = User.where(
+            "username = :username",
+            { :username => params[:username] })
+    @post = Post.new(params[:post])
+    @post.user_id = user
+    @post.create_date = Time.new.strftime('%Y-%m-%d')
+    
+    
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to(@post,
+                       :notice => 'Successfully created user.') }
+        format.json { render :json => @post,
+                       :status => :created, :location => @post }
+      else
+        format.html { render :action => 'new' }
+        format.json { render :action => @post.errors,
+                      :status => :unprocessable_entity }                 
+      end
+    end
+  end
+  
+  def new
+    @post = Post.new
+    
+    respond_to do |format|
+      format.html #new.html.erb
+      format.json { render :json => @user }
+    end
+  end
+end
