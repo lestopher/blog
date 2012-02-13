10a8af6866f6e73fd346fd228254104a16c18aad
Christopher Nguyen <christoph.nguyen@gmail.com>
2012-02-13 00:20:22 -0800

    Changed blob to binary for postgres

--- db/migrate/20120213081138_create_posts.rb
+++ db/migrate/20120213081138_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :post_title
-      t.blob :post
+      t.binary :post
       t.integer :user_id
       t.date    :create_date
       t.timestamps
