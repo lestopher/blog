6301920172f32e11f12af19f91918212a488fc56
Christopher Nguyen <christoph.nguyen@gmail.com>
2012-02-14 09:53:05 -0800

    Moved encrypt code from UsersController to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :fname, :lname, :username, :password, :on => :create
   validates_uniqueness_of :username
+  
+   before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  private
+    
+    def encrypt_password
+      self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(submitted_password)
+    end
+    
+    def encrypt(string)
+      secure_hash('#{salt}--#{string}')
+    end
+    
+    def make_salt
+      secure_hash('#{Time.now.utc}--#{password}')
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+    
 end
